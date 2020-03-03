c8ace09b7837ec9ae3303e9a9a506bc76e47b7b1
Zach Wendell <zach.wendell@megaport.com>
2020-03-03 12:10:46 -0800

    updated script and cleaned up some of the code

--- edgeos_wan_ip_updater.py
+++ edgeos_wan_ip_updater.py
@@ -3,24 +3,21 @@
 ##### WAN IP UPDATER #####
 #
 # This script should be run automatically via cron and update tunnel config
-# with the current WAN IP address, as our ISP changes the IP on
+# with the current WAN IPv4 address, as our ISP changes the DHCP IP on
 # disconnect/reconnect.
 #
 # Currently, reluctantly, using Python 2, as that's the default Python on
 # Ubiquiti EdgeOS at the moment. Queue complaining and shaming on 2020-01-01!
 #
 # Written by zmw, 201912
-# Last Updated: 20200303T033130Z
+# Last Updated: 20200303T201026Z
 
 
 # IMPORT LIBRARIES
-import fcntl
 import json
 import os
 #import requests
 import signal
-import socket
-import struct
 import subprocess
 import sys
 
@@ -40,20 +37,22 @@ wrap_cli_api = 'cli-shell-api '
 # Functions for getting the current WAN IP, getting a list of tunnel
 # interfaces, as well as adding/changing/updating configuration...
 def get_wan_ip():
-  # Get list of Interfaces and their current IP addresses
-  response = os.popen(wrap_oper + 'show interfaces').read()
-  # Extract pppoe0 interface IP
-  for line in response.splitlines():
-    if 'pppoe0' in line:
-      get_wan_ip.wan_ip = line.split()[1]
+  # Using Linux CLI, get current info for pppoe0 interface
+  ip_output = os.popen('ip -4 a show pppoe0').read().split('\n')
+  # Extract just the IPv4 address
+  wan_ip = [item for item in ip_output if 'inet' in item][0].strip().split(' ')[1]
+  # Return WAN IP
+  return wan_ip
 
-def get_tun_ints():
+def get_tun_ifaces():
   response = os.popen(wrap_oper + 'show interfaces').read()
   # Extract tunnel interfaces into a list
-  get_tun_ints.tun_ints = []
+  tun_ifaces = []
   for line in response.splitlines():
     if 'tun' in line:
-      get_tun_ints.tun_ints.append(line.split()[0])
+      tun_ifaces.append(line.split()[0])
+  # Return list of tunnel interfaces
+  return tun_ifaces
 
 def configure_router(config_set):
   for cmd in config_set:
@@ -66,33 +65,23 @@ def update_he_tunnelbroker():
     he_username = secrets['he_tunnelbroker']['username']
     he_update_key = secrets['he_tunnelbroker']['update_key']
     he_tunnel_id = secrets['he_tunnelbroker']['tunnel_id']
+  # Get current WAN IP address
+  wan_ip = get_wan_ip()
   # Define HE TunnelBroker Update URI
-  he_tunnelbroker_uri = 'https://' + he_username + ':' + he_update_key + '@ipv4.tunnelbroker.net/nic/update?hostname=' + he_tunnel_id + '&myip=' + get_wan_ip.wan_ip 
+  he_tunnelbroker_uri = 'https://' + he_username + ':' + he_update_key + '@ipv4.tunnelbroker.net/nic/update?hostname=' + he_tunnel_id + '&myip=' + wan_ip 
   # HTTP GET to HE TunnelBroker Update URI to trigger update with specified IP address
   requests.get(he_tunnelbroker_uri)
 
 def centurylink_6rd():
-  # Steps:
   # 0. Initial setup of EdgeOS firewall rules, tunnel interface, LAN interface, etc. on router
-  # 1. Get v4 WAN IP from pppoe0
-  # 2. Calculate v6 RD prefix (/56)
-  # 3. Assign address to tunnel interface
-  # 4. Assign address to LAN interface
-  # 5. Define routes
-  # 6. Test connectivity / DONE!
+  # 1. Get v4 WAN IP from pppoe0 & calculate v6 RD prefix (/56) and other v6 addresses
+  # 2. Assign addresses to tunnel & LAN interfaces
+  # 3. Define routes & test connectivity
   #
-  # Use socket, fcntl & struct libraries to obtain WAN IP
-  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  # Return IPv4 WAN IP address
-  ipv4addr = socket.inet_ntoa(
-    fcntl.ioctl(
-      s.fileno(),
-      0x8915, # SIOCGIFADDR
-      struct.pack('256s', 'pppoe0'[:15]) # Change from pppoe0 if using another interface
-    )[20:24]
-  )
+  # Get current WAN IPv4 address
+  wan_ip = get_wan_ip()
   # Split IPv4 address into octets
-  v4parts = ipv4addr.split('.')
+  v4parts = wan_ip.split('.')
   # Define IPv6 address parts as a list and begin with '2602', as that will always be the first
   # part of the resulting IPv6 address
   v6parts = ['2602']
@@ -107,22 +96,26 @@ def centurylink_6rd():
       if len(val) < 2:
         v6parts[idx] = '0' + val
   # Base IPv6 block to build other subnets from
-  myv6_prefix = v6parts[0] + ':' + v6parts[1] + ':' + v6parts[2] + v6parts[3] + ':' +  v6parts[4] + '00'
+  myv6_base = v6parts[0] + ':' + v6parts[1] + ':' + v6parts[2] + v6parts[3] + ':' +  v6parts[4] + '00'
   # Entire /56 prefix
-  myv6_block = myv6_prefix + '::' + '/56'
+  myv6_prefix = myv6_base + '::' + '/56'
   # Tunnel interface /128
-  myv6_wan_128 = myv6_prefix + '::1/128'
+  myv6_wan_128 = myv6_base + '::1/128'
   # 1st LAN /64
-  myv6_lan_64_1 = myv6_prefix[:-1] + '1::/64'
+  myv6_lan_64_1 = myv6_base[:-1] + '1::/64'
   # 2nd LAN /64
-  myv6_lan_64_2 = myv6_prefix[:-1] + '2::/64'
+  myv6_lan_64_2 = myv6_base[:-1] + '2::/64'
   # 3rd LAN /64
-  myv6_lan_64_3 = myv6_prefix[:-1] + '3::/64'
+  myv6_lan_64_3 = myv6_base[:-1] + '3::/64'
+  # 4th LAN /64
+  myv6_lan_64_4 = myv6_base[:-1] + '4::/64'
+  # 5th LAN /64
+  myv6_lan_64_5 = myv6_base[:-1] + '5::/64'
   # Build a dictionary of the values to be returned when running this function
   cl_6rd_dict = {
-    'v4addr': ipv4addr,
-    'v6tunnel': myv6_wan_128,
-    'v6lan': myv6_lan_64_1
+    'v4wan': wan_ip,
+    'v6rdtun': myv6_wan_128,
+    'v6lan1': myv6_lan_64_1
   }
   # Return dict
   return cl_6rd_dict
@@ -131,24 +124,24 @@ def centurylink_6rd():
 # Main Function
 def main():
   # Get current WAN IP address
-  get_wan_ip()
+  wan_ip = get_wan_ip()
   # Get list of tunnel interfaces
-  get_tun_ints()
+  tun_ifaces = get_tun_ifaces()
   # Iterate through tunnel interfaces and update local-ip to current WAN IP
-  for tun_int in get_tun_ints.tun_ints:
+  for tun_iface in tun_ifaces:
     # Configuration command set
     config_set = [
       wrap_conf + 'begin',
-      wrap_conf + 'set interfaces tunnel ' + tun_int + ' local-ip ' + get_wan_ip.wan_ip,
+      wrap_conf + 'set interfaces tunnel ' + tun_iface + ' local-ip ' + wan_ip,
       wrap_conf + 'commit',
       wrap_conf + 'save'
     ]
     # Run configure_router function with config_set
     try:
       configure_router(config_set)
-      print(tun_int + ' updated with current WAN IP (' + get_wan_ip.wan_ip + ')')
+      print(tun_iface + ' updated with current WAN IP (' + wan_ip + ')')
     except:
-      print('Unable to configure ' + tun_int + ' with current WAN IP.')
+      print('Unable to configure ' + tun_iface + ' with current WAN IP.')
 
 
 # RUN MAIN FUNCTION
